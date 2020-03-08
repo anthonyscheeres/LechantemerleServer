85382324389a544b6697285c6870b72e396fa22c
Anthony <info@anthonyscheeres.nl>
2020-03-08 02:28:24 +0100

    .

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,6 +8,7 @@ namespace ChantemerleApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        PermissionService permissionService = new PermissionService();
         UserService userService = new UserService();
 
         // GET: api/User/showAllUsers
@@ -44,6 +45,18 @@ namespace ChantemerleApi.Controllers
         {
  
             return userService.validatDeleteUserByModel(token, user);
+        }
+
+
+  
+        // POST: api/User/login
+        [Route("login")]
+        [HttpPost]
+
+        public string loginUser([FromBody] UserModel user)
+        {
+            //ask the permission layer what permission this user has after an validation
+            return permissionService.loginUserAfterValidation(user);
         }
     }
 }
